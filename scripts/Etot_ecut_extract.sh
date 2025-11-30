32640c72146b85010fb1b1816a34faf1b48f67d2
Ravindu Kalhara <kalhara.ravindu2000@gmail.com>
2025-12-01 00:33:21 +0530

    feat: script to extract total energy and ecutwfc

---
+++ scripts/Etot_ecut_extract.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+output="Etot_ecut.dat"
+: > "$output"	# empty the file
+
+for file in phl.ecut_*.out; do
+    ecutwfc=$(echo "$file" | sed 's/.*ecut_//; s/\.out$//')	 # extract ecutwfc
+    total_energy=$(grep -e '^!.*total energy' "$file" | awk '{print $5}')	# extract total energy
+
+    line="${ecutwfc}\t\t${total_energy}"
+    printf "%b\n" "$line"	# print to terminal
+    printf "%b\n" "$line" >> "$output"	# append to file
+done
+
+echo "Saved results to $output"
