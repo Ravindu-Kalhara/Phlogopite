d69993df294acca9f5b510b3a70f572a18e85acd
Ravindu Kalhara <kalhara.ravindu2000@gmail.com>
2025-12-18 13:25:27 +0530

    perf: improved vc_relax of phl-fe0-dftd3

--- vc_relax/phl-fe0-dftd3/get_reciprocal.py
+++ vc_relax/phl-fe0-dftd3/get_reciprocal.py
@@ -45,7 +45,7 @@ def reciprocal_lattice_vectors(lattice_vectors):
     return b1, b2, b3
 
 
-filename = 'phl-fe0-vc_relax.vasp'
+filename = 'phl-fe0-dftd3-vc_relax.vasp'
 
 try:
     lattice_vectors = read_vasp_file(filename)
